ccb14c6158c7c01e99a3f2bb4684b3af58b32532
Ignacio Cunial <igsecu@hotmail.com>
2023-08-09 17:53:56 -0300

    Created GET single member route

--- routes/members.js
+++ routes/members.js
@@ -9,4 +9,21 @@ router.get("/", (req, res) => {
   res.status(200).json(members);
 });
 
+// Get single member
+router.get("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.status(200).json(
+      members.filter((member) => {
+        return member.id === parseInt(req.params.id);
+      })
+    );
+  } else {
+    res.status(404).json({
+      msg: `Member with the id: ${req.params.id} not found!`,
+    });
+  }
+});
+
 module.exports = router;
