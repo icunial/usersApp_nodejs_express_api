ad42f6ad0dc6860ad07d0d4f7de1cb7f8650031e
Ignacio Cunial <igsecu@hotmail.com>
2023-08-09 10:47:53 -0300

    Created Members.js -> members array objects hardcoded

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Name 1",
+    email: "name1@email.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Name 2",
+    email: "name2@email.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Name 3",
+    email: "name3@email.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
